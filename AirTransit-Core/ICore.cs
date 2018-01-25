9a1f9ba90454554adccb249df32f601a11d8427d
fvigneault <minivigno@gmail.com>
2018-01-25 16:30:41 -0500

    Changed solution name and interface for core

--- AirTransit-Core/ICore.cs
+++ AirTransit-Core/ICore.cs
@@ -6,8 +6,13 @@ namespace AirTransit_Core
 {
     interface ICore
     {
-        String SendMessage(String phoneNumber, String message);
-        String GetMessages();
-        String DeleteMessages();
+        Message SendMessage(String phoneNumber, String message);
+        List<Message> GetMessages();
+        List<Message> GetMessages(DateTime since);
+        List<Message> GetMessages(int maximumNumberOfMessages);
+        List<Message> GetMessages(String phoneNumber);
+        List<Message> GetMessages(String phoneNumber, DateTime since);
+        List<Message> GetMessages(String phoneNumber, int maximumNumberOfMessages);
+        bool DeleteMessages(List<String> messageIDs);
     }
 }
