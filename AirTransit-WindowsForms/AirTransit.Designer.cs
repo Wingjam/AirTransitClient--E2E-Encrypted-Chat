6530a287cbcb77f38cfd3a759c3f26a60c827b3e
jeepee12 <jeepee.12@hotmail.com>
2018-01-25 17:18:35 -0500

    Adding UI elements to the windows form

--- AirTransit-WindowsForms/AirTransit.Designer.cs
+++ AirTransit-WindowsForms/AirTransit.Designer.cs
@@ -28,12 +28,121 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.MenuStrip = new System.Windows.Forms.MenuStrip();
+            this.menuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.TxtContactList = new System.Windows.Forms.RichTextBox();
+            this.Txt = new System.Windows.Forms.TextBox();
+            this.Txtconversation = new System.Windows.Forms.RichTextBox();
+            this.groupBox1.SuspendLayout();
+            this.MenuStrip.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.TxtContactList);
+            this.groupBox1.Location = new System.Drawing.Point(12, 27);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(212, 477);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Contacts";
+            // 
+            // MenuStrip
+            // 
+            this.MenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuToolStripMenuItem});
+            this.MenuStrip.Location = new System.Drawing.Point(0, 0);
+            this.MenuStrip.Name = "MenuStrip";
+            this.MenuStrip.Size = new System.Drawing.Size(861, 24);
+            this.MenuStrip.TabIndex = 1;
+            this.MenuStrip.Text = "menuStrip1";
+            // 
+            // menuToolStripMenuItem
+            // 
+            this.menuToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.menuToolStripMenuItem.Name = "menuToolStripMenuItem";
+            this.menuToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.menuToolStripMenuItem.Text = "Menu";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.Txtconversation);
+            this.groupBox2.Controls.Add(this.Txt);
+            this.groupBox2.Location = new System.Drawing.Point(230, 27);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(619, 477);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Messages";
+            // 
+            // TxtContactList
+            // 
+            this.TxtContactList.Location = new System.Drawing.Point(7, 20);
+            this.TxtContactList.Name = "TxtContactList";
+            this.TxtContactList.ReadOnly = true;
+            this.TxtContactList.Size = new System.Drawing.Size(199, 451);
+            this.TxtContactList.TabIndex = 0;
+            this.TxtContactList.Text = "";
+            // 
+            // Txt
+            // 
+            this.Txt.Location = new System.Drawing.Point(6, 424);
+            this.Txt.Multiline = true;
+            this.Txt.Name = "Txt";
+            this.Txt.Size = new System.Drawing.Size(607, 47);
+            this.Txt.TabIndex = 0;
+            // 
+            // Txtconversation
+            // 
+            this.Txtconversation.Location = new System.Drawing.Point(6, 19);
+            this.Txtconversation.Name = "Txtconversation";
+            this.Txtconversation.Size = new System.Drawing.Size(607, 399);
+            this.Txtconversation.TabIndex = 1;
+            this.Txtconversation.Text = "";
+            // 
+            // AirTransit
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(861, 516);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.MenuStrip);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MainMenuStrip = this.MenuStrip;
+            this.Name = "AirTransit";
+            this.Text = "AirTransit";
+            this.groupBox1.ResumeLayout(false);
+            this.MenuStrip.ResumeLayout(false);
+            this.MenuStrip.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.MenuStrip MenuStrip;
+        private System.Windows.Forms.ToolStripMenuItem menuToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RichTextBox TxtContactList;
+        private System.Windows.Forms.RichTextBox Txtconversation;
+        private System.Windows.Forms.TextBox Txt;
     }
 }
 
