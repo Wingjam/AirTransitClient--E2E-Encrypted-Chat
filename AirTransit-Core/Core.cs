e39723cb818c0fea2621300962a756deaff94b0a
fvigneault <minivigno@gmail.com>
2018-02-01 15:55:52 -0500

    temp code for Core.cs

--- AirTransit-Core/Core.cs
+++ AirTransit-Core/Core.cs
@@ -1,9 +1,34 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace AirTransit_Core
 {
     public class Core
     {
+        private readonly Task _getMessagesTask;
+        private bool Running = false;
 
+        public Core(Action<IEnumerable<String>> notifyAction)
+        {
+            this._getMessagesTask = new Task(() =>
+            {
+                while (Running)
+                    GetMessages(notifyAction);
+            });
+        }
+
+
+        public void Stop()
+        {
+            Running = false;
+            this._getMessagesTask.Wait();
+        }
+
+        public void GetMessages(Action<IEnumerable<String>> notifyAction)
+        {
+            var messages = new string[] { "yolo", "1234" };
+            notifyAction(messages);
+        }
     }
 }
