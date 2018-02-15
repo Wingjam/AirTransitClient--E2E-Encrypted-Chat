c96533af0457a7fea9314f3d2c0250abcf95f7e1
Raphaël Cayouette <raphael.cayouette@usherbrooke.ca>
2018-02-15 14:37:51 -0500

    Added console application implementation using core services

---
+++ AirTransit-Console/AirTransitConsole.cs
@@ -0,0 +1,285 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AirTransit_Core.Models;
+using AirTransit_Core;
+
+namespace AirTransit_Console
+{
+    public class AirTransitConsole
+    {
+        private LoggedInUser _loggedInUser;
+        private CoreServices _coreServices;
+        private Contact _currentContact;
+        private const bool TEXTINPUTFOLLOWING = true;
+        private const string VERSION = "1.0";
+
+        public AirTransitConsole()
+        {
+            _coreServices = new CoreServices();
+        }
+        
+        public void AppLoop()
+        {
+            var mustQuit = false;
+            var loggedIn = false;
+            WriteDecoratedText($"AirTransit V{VERSION}");
+            while (!loggedIn)
+            {
+                var phoneNumber = GetPhoneInput("Please enter your phone number (10 digits): ");
+                loggedIn = _coreServices.Init(phoneNumber);
+            }
+            ShowMenuOptions();
+            while (!mustQuit)
+            {
+                var command = GetTextInput("\nEnter your command: ");
+                switch (command?.ToUpper())
+                {
+                    case "SM": //SendMessage
+                        SendMessage();
+                        break;
+                    case "FM": //FetchMessages
+                        FetchMessages();
+                        break;
+                    case "SC": //SelectContact
+                        SelectContact();
+                        FetchMessages();
+                        break;
+                    case "AC": //AddContact
+                        AddContact();
+                        break;
+                    case "ST": //ShowContacts
+                        ShowContacts();
+                        break;
+                    case "DC": //DeleteContact
+                        DeleteContact();
+                        break;
+                    case "MO": //ShowMenuOptions
+                    case "HELP":
+                        ShowMenuOptions();
+                        break;
+                    case "QQ": //Quit
+                        WriteToConsole("AirTransit closing...");
+                        mustQuit = true;
+                        break;
+                    default: //Invalid
+                        WriteInvalidInput();
+                        break;
+                }
+            }
+            Environment.Exit(0);
+        }
+
+        private static string GetTextInput(string text)
+        {
+            string result;
+            do
+            {
+                WriteToConsole(text, TEXTINPUTFOLLOWING);
+                result = Console.ReadLine();
+            } while (string.IsNullOrEmpty(result));
+
+            return result;
+        }
+        
+        private static int GetIntInput(string text)
+        {
+            var result = 0;
+            string input;
+            do
+            {
+                WriteToConsole(text, TEXTINPUTFOLLOWING);
+                input = Console.ReadLine();
+            } while (!int.TryParse(input, out result));
+
+            return result;
+        }
+        
+        private static string GetPhoneInput(string text)
+        {
+            string result;
+            do
+            {
+                WriteToConsole(text, TEXTINPUTFOLLOWING);
+                result = Console.ReadLine();
+            } while (string.IsNullOrEmpty(result) || result.Length != 10 || !result.All(char.IsDigit));
+
+            return result;
+        }
+
+        private void SendMessage()
+        {
+            if (_currentContact == null)
+            {
+                SelectContact();
+            }
+            var answer = GetTextInput($"Send message to {_currentContact.Name} " +
+                                      $"({_currentContact.PhoneNumber})? (y/n): ");
+            if (answer.ToLower() == "n")
+            {
+                SelectContact();
+            }
+            var message = GetTextInput($"Message to {_currentContact.Name}: ");
+            _coreServices.MessageService.SendMessage(_currentContact, message);
+        }
+
+        private void FetchMessages()
+        {
+            if (_currentContact == null)
+            {
+                SelectContact();
+            }
+            WriteDecoratedText($"{_currentContact.Name} ({_currentContact.PhoneNumber})");
+            var fetchMoreMessages = true;
+            var messagesToFetch = 10;
+            do
+            {
+                var messages = messagesToFetch == -1 
+                    ? _coreServices.MessageRepository.GetMessages(_currentContact) 
+                    : _coreServices.MessageRepository.GetMessages(_currentContact, messagesToFetch);
+                foreach (var message in messages)
+                {
+                    WriteToConsole($"{message.Timestamp} - {message.Content}");
+                }
+                var getLongerHistory = GetTextInput($"Get longer message history? (y/n/all): ").ToLower();
+                switch (getLongerHistory)
+                {
+                    case "n":
+                        messagesToFetch += 10;
+                        WriteDecoratedText($"{_currentContact.Name} ({_currentContact.PhoneNumber})");
+                        break;
+                    case "all":
+                        messagesToFetch = -1;
+                        WriteDecoratedText($"{_currentContact.Name} ({_currentContact.PhoneNumber})");
+                        break;
+                    default:
+                        fetchMoreMessages = false;
+                        break;
+                }
+            } while (fetchMoreMessages);
+        }
+
+        private void AddContact()
+        {
+            WriteDecoratedText("Add a contact");
+            var name = GetTextInput("Contact name: ");
+            var phoneNumber = GetPhoneInput("Phone number (10 digits): ");
+            _coreServices.ContactRepository.AddContact(new Contact(phoneNumber, name));
+            WriteToConsole($"{name} added to your contacts.");
+        }
+
+        private void SelectContact()
+        {
+            WriteDecoratedText("Select a contact");
+            var contact = ContactSelection();
+            if (contact == null) return;
+            _currentContact = contact;
+            WriteDecoratedText($"{_currentContact.Name} ({_currentContact.PhoneNumber})");
+        }
+
+        private void DeleteContact()
+        {
+            WriteDecoratedText("Delete a contact");
+            var contact = ContactSelection();
+            if (contact == null) return;
+            _coreServices.ContactRepository.DeleteContact(contact);
+            WriteToConsole($"{contact.Name} removed from your contacts.");
+        }
+
+        private Contact ContactSelection()
+        {
+            Contact result = null;
+            var contacts = _coreServices.ContactRepository.GetContacts().ToList();
+            if (contacts.Count <= 0) return null;
+            
+            do
+            {
+                for (var i = 0; i < contacts.Count; ++i)
+                {
+                    WriteToConsole($"{i}. {contacts[i].Name} ({contacts[i].PhoneNumber})");
+                }
+                var contactId = GetIntInput($"Choose a contact (0-{contacts.Count - 1}): ");
+                if (contactId < 0 || contactId > contacts.Count - 1)
+                {
+                    WriteInvalidInput();
+                    continue;
+                }; 
+                result = contacts[contactId];
+            } while (result == null);
+
+            return result;
+        }
+
+        private void ShowContacts()
+        {
+            WriteDecoratedText("Your contacts");
+            var contacts = _coreServices.ContactRepository.GetContacts().ToList();
+            foreach (var contact in contacts)
+            {
+                WriteToConsole($"{contact.Name} ({contact.PhoneNumber})");
+            }
+        }
+        
+        private static string DecorateText(string text)
+        {
+            const string DECORATION = "------------------------------";
+            var sbuild = new StringBuilder();
+            sbuild.AppendLine(DECORATION);
+            sbuild.AppendLine(text.PadLeft(text.Length+(30-text.Length)/2));
+            sbuild.AppendLine(DECORATION);
+            return sbuild.ToString();
+        }
+
+        private static void WriteToConsole(string text, bool textInputFollowing = false)
+        {
+            if (string.IsNullOrEmpty(text)) return;
+            if (!textInputFollowing)
+            {
+                Console.WriteLine(text);
+            }
+            else
+            {
+                Console.Write(text);
+            }
+        }
+
+        private static void WriteInvalidInput()
+        {
+            WriteToConsole("Invalid input.");
+        }
+
+        private static void WriteDecoratedText(string text)
+        {
+            WriteToConsole(DecorateText(text));
+        }
+
+        private static void ShowMenuOptions()
+        {
+            var sbuild = new StringBuilder(DecorateText("Menu options"));
+            var options = new List<string>
+            {
+                "SM - Send Message",
+                "FM - Fetch messages",
+                "SC - Select contact",
+                "AC - Add contact",
+                "ST - Show contacts",
+                "DC - Delete contact",
+                "MO - Show menu options",
+                "QQ - Quit"
+            };
+            foreach (var option in options)
+            {
+                sbuild.AppendLine(option);
+            }
+            WriteToConsole(sbuild.ToString(), true);
+        }
+
+        private class LoggedInUser
+        {
+            public string PhoneNumber { get; set; }
+            public List<Message> Messages { get; set; }
+            public List<Contact> Contacts { get; set; }
+        }
+    }
+}
--- AirTransit-Console/ConsoleApp.cs
+++ AirTransit-Console/ConsoleApp.cs
@@ -1,24 +1,11 @@
-﻿using System;
-using System.Threading;
-using AirTransit_Core;
-
-namespace AirTransit_Console
+﻿namespace AirTransit_Console
 {
-    class ConsoleApp
+    public class ConsoleApp
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            var messageFetcher = new MessageFetcher((messages) =>
-            {
-                Thread.Sleep(new TimeSpan(0, 0, 0, 5));
-                foreach (var message in messages)
-                {
-                    Console.WriteLine(message);
-
-                }
-            }, new TimeSpan(0,0,0,1));
-            
-            Console.Read();
+            var app = new AirTransitConsole();
+            app.AppLoop();
         }
     }
 }
