a92160d92a506d9a7d499c327d694269e5fc3adb
jeepee12 <jeepee.12@hotmail.com>
2018-03-15 15:37:19 -0400

    Adds the MessageDTO.

---
+++ AirTransit-Core/Models/MessageDTO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace AirTransit_Core.Models
+{
+    internal class MessageDTO
+    {
+        public string Id { get; set; }
+
+        /// <summary>
+        /// Is in the signature, that is in the content of the encrypted message.
+        /// </summary>
+        public string SenderPhoneNumber { get; set; }
+
+        public string DestinationPhoneNumber { get; set; }
+
+        /// <summary>
+        /// Is in the content
+        /// </summary>
+        public DateTime Timestamp { get; set; }
+
+        public string Content { get; set; }
+    }
+}
