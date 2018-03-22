8f3c00ee669ed1ba85c72ae849f87b8123add52b
Jonathan Martineau <jo_martineau_7@hotmail.com>
2018-03-22 16:32:54 -0400

    Added unit test for long message encryption

--- AirTransit_Core_Tests/Services/RSAEncryptionServiceTests.cs
+++ AirTransit_Core_Tests/Services/RSAEncryptionServiceTests.cs
@@ -54,6 +54,23 @@ namespace AirTransit_Core_Tests.Services
 
         }
 
+        [Fact]
+        public void EncryptLongMessage_CanBeDecryptedSuccessfully()
+        {
+            var keySet = GenerateValidRsaKeySet();
+            A.CallTo(() => this._keySetRepository.GetKeySet()).Returns(keySet);
+            var longMessage = "{\"Content\":\"allo\",\"SenderPhoneNumber\":\"8198888888\",\"Signature\":\"imB0iiYKTr4lxaXdSRp/Wyuyqk2jjT6EdWlRaSCjPYrPZwsy7JT8VuixqLSyTsbehyW9nT3bARxVpadl0VZ9qfBfyOojyM75qkJRY0a+8oyaRXM7ahFreZR3jrGVbGV+ZhvmbvabQnLdmSZ2LAEhbWUfaZBx1uQfB5/I48zRMNZ7NUARK9f1etdRzPWWO3ApvdgrSmasaZ5N5xPtp6oU0jWOF9EF9q4kDQhj4rnYSBmS7tOwkhZ7Mj6ywe+HkSM2hGSCGSvShXPqtFB3E3Mp/MyiqVHsV2fzqGOkBwtM6zf7S5lWqBl5PGPx4SFqk5uSGVuLKOVOTXv65pIPmpUprA==\",\"Timestamp\":\"2018-03-22T16:29:14.317983-04:00\"}";
+
+            var contact = new Contact
+            {
+                PublicKey = keySet.PublicKey
+            };
+
+            var encryptedMessage = this._rsaEncryptionService.Encrypt(longMessage, contact);
+            Assert.Equal(longMessage, this._rsaEncryptionService.Decrypt(encryptedMessage));
+
+        }
+
         #region Helpers
         private static KeySet GenerateValidRsaKeySet()
         {
